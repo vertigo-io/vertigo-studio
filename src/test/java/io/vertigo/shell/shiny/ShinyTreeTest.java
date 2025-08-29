d43e56772e416ca70b8304eb8f2b06676505734f
phlppchrtn <philippe.chretien@kleegroup.com>
2025-08-29 13:23:30 +0200

    tree > set

--- src/test/java/io/vertigo/shell/shiny/ShinyTreeTest.java
+++ src/test/java/io/vertigo/shell/shiny/ShinyTreeTest.java
@@ -14,17 +14,17 @@ public class ShinyTreeTest {
 		System.out.println(ShinyColors.BLUE_BRIGHT + "--- Video Games Tree ---" + ShinyColors.RESET);
 		final var tree = Shiny.tree("Video Games");
 		tree.getRoot()
-				.addNode("RPG", ShinyIcon.FOLDER_OPEN)
-				.addNode("Final Fantasy", ShinyIcon.FILE)
-				.addNode("The Elder Scrolls", ShinyIcon.FILE);
+				.addChild("RPG", ShinyIcon.FOLDER_OPEN)
+				.addChild("Final Fantasy", ShinyIcon.FILE).up()
+				.addChild("The Elder Scrolls", ShinyIcon.FILE);
 		tree.getRoot()
-				.addNode("FPS", ShinyIcon.FOLDER_OPEN)
-				.addNode("Halo", ShinyIcon.FILE)
-				.addNode("Call of Duty", ShinyIcon.FILE);
+				.addChild("FPS", ShinyIcon.FOLDER_OPEN)
+				.addChild("Halo", ShinyIcon.FILE).up()
+				.addChild("Call of Duty", ShinyIcon.FILE);
 		tree.getRoot()
-				.addNode("Adventure", ShinyIcon.FOLDER_OPEN)
-				.addNode("The Legend of Zelda", ShinyIcon.FILE)
-				.addNode("Uncharted", ShinyIcon.FILE);
+				.addChild("Adventure", ShinyIcon.FOLDER_OPEN)
+				.addChild("The Legend of Zelda", ShinyIcon.FILE).up()
+				.addChild("Uncharted", ShinyIcon.FILE);
 
 		tree.print();
 		System.out.println();
@@ -34,24 +34,24 @@ public class ShinyTreeTest {
 		System.out.println(ShinyColors.BLUE_BRIGHT + "--- Icon Showcase ---" + ShinyColors.RESET);
 		final var tree = Shiny.tree("Icons");
 		tree.getRoot()
-				.addNode("Status Icons", ShinyIcon.FOLDER_OPEN)
-				.addNode("Success", ShinyIcon.SUCCESS)
-				.addNode("Error", ShinyIcon.ERROR)
-				.addNode("Warning", ShinyIcon.WARNING)
-				.addNode("Info", ShinyIcon.INFO)
-				.addNode("Question", ShinyIcon.QUESTION);
+				.addChild("Status Icons", ShinyIcon.FOLDER_OPEN)
+				.addChild("Success", ShinyIcon.SUCCESS).up()
+				.addChild("Error", ShinyIcon.ERROR).up()
+				.addChild("Warning", ShinyIcon.WARNING).up()
+				.addChild("Info", ShinyIcon.INFO).up()
+				.addChild("Question", ShinyIcon.QUESTION).up();
 		tree.getRoot()
-				.addNode("Time & Arrows", ShinyIcon.FOLDER_OPEN)
-				.addNode("Clock", ShinyIcon.CLOCK)
-				.addNode("Up", ShinyIcon.ARROW_UP)
-				.addNode("Down", ShinyIcon.ARROW_DOWN)
-				.addNode("Left", ShinyIcon.ARROW_LEFT)
-				.addNode("Right", ShinyIcon.ARROW_RIGHT);
+				.addChild("Time & Arrows", ShinyIcon.FOLDER_OPEN)
+				.addChild("Clock", ShinyIcon.CLOCK).up()
+				.addChild("Up", ShinyIcon.ARROW_UP).up()
+				.addChild("Down", ShinyIcon.ARROW_DOWN).up()
+				.addChild("Left", ShinyIcon.ARROW_LEFT).up()
+				.addChild("Right", ShinyIcon.ARROW_RIGHT).up();
 		tree.getRoot()
-				.addNode("Misc Icons", ShinyIcon.FOLDER_OPEN)
-				.addNode("Star", ShinyIcon.STAR)
-				.addNode("Heart", ShinyIcon.HEART)
-				.addNode("Smiley", ShinyIcon.SMILEY);
+				.addChild("Misc Icons", ShinyIcon.FOLDER_OPEN)
+				.addChild("Star", ShinyIcon.STAR).up()
+				.addChild("Heart", ShinyIcon.HEART).up()
+				.addChild("Smiley", ShinyIcon.SMILEY).up();
 		tree.print();
 		System.out.println();
 	}
@@ -59,20 +59,21 @@ public class ShinyTreeTest {
 	private static void testComplexTreeStructure() {
 		System.out.println(ShinyColors.BLUE_BRIGHT + "--- Complex Tree Structure ---" + ShinyColors.RESET);
 		final var tree = Shiny.tree("Project Structure");
-		final var srcNode = tree.getRoot().addNode("src", ShinyIcon.FOLDER_CLOSED);
-		srcNode.addNode("main", ShinyIcon.FOLDER_CLOSED)
-				.addNode("java", ShinyIcon.FOLDER_CLOSED)
-				.addNode("com.example.app", ShinyIcon.FOLDER_CLOSED)
-				.addNode("Application.java", ShinyIcon.FILE);
-		srcNode.addNode("resources", ShinyIcon.FOLDER_CLOSED)
-				.addNode("application.properties", ShinyIcon.FILE);
-		final var testNode = tree.getRoot().addNode("test", ShinyIcon.FOLDER_CLOSED);
-		testNode.addNode("java", ShinyIcon.FOLDER_CLOSED)
-				.addNode("com.example.app", ShinyIcon.FOLDER_CLOSED)
-				.addNode("ApplicationTest.java", ShinyIcon.FILE);
-		tree.getRoot().addNode("pom.xml", ShinyIcon.FILE);
-		tree.getRoot().addNode("database", ShinyIcon.DB);
-		tree.getRoot().addNode("admin", ShinyIcon.USER);
+		final var srcNode = tree.getRoot().addChild("src", ShinyIcon.FOLDER_CLOSED);
+		var main = srcNode.addChild("main", ShinyIcon.FOLDER_CLOSED);
+		var java = main.addChild("java", ShinyIcon.FOLDER_CLOSED);
+		java.addChild("com.example.app", ShinyIcon.FOLDER_CLOSED);
+		java.addChild("Application.java", ShinyIcon.FILE);
+		var resources = srcNode.addChild("resources", ShinyIcon.FOLDER_CLOSED);
+		resources.addChild("application.properties", ShinyIcon.FILE);
+		final var testNode = tree.getRoot().addChild("test", ShinyIcon.FOLDER_CLOSED);
+		var javaTest = testNode.addChild("java", ShinyIcon.FOLDER_CLOSED);
+		javaTest.addChild("com.example.app", ShinyIcon.FOLDER_CLOSED);
+		javaTest.addChild("ApplicationTest.java", ShinyIcon.FILE);
+
+		tree.getRoot().addChild("pom.xml", ShinyIcon.FILE);
+		tree.getRoot().addChild("database", ShinyIcon.DB);
+		tree.getRoot().addChild("admin", ShinyIcon.USER);
 
 		tree.print();
 		System.out.println();
