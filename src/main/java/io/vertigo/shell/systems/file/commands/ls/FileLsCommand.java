4bf7bdcc62620153f1950fe0f32d848f21b73380
phlppchrtn <philippe.chretien@kleegroup.com>
2025-09-15 17:30:00 +0200

    2 steps

--- src/main/java/io/vertigo/shell/systems/file/commands/ls/FileLsCommand.java
+++ src/main/java/io/vertigo/shell/systems/file/commands/ls/FileLsCommand.java
@@ -3,10 +3,14 @@ package io.vertigo.shell.systems.file.commands.ls;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import io.vertigo.shell.ShellCommand;
 import io.vertigo.shell.systems.file.FileContext;
+import io.vertigo.shiny.Shiny;
+import io.vertigo.shiny.ShinyWriter;
 import picocli.CommandLine.Command;
 
 @Command(name = "ls", description = "List files and directories.")
@@ -16,13 +20,27 @@ public final class FileLsCommand implements ShellCommand {
 	public void run() {
 		final FileContext fileContext = FileContext.get();
 		final Path path = fileContext.getCurrentAbsolutePath();
+
+		final List<String[]> rows = new ArrayList<>();
+
 		try (Stream<Path> stream = Files.list(path)) {
 			stream.forEach(p -> {
-				final String color = Files.isDirectory(path) ? "34" : "0"; // blue for directories
-				System.out.println("\u001B[" + color + "m" + p.getFileName().toString() + "\u001B[0m");
+				final String[] row = new String[2];
+				row[0] = p.getFileName().toString();
+				row[1] = Boolean.valueOf(!Files.isDirectory(p)).toString();
+				rows.add(row);
 			});
 		} catch (final IOException e) {
 			System.out.println("Error listing files: " + e.getMessage());
 		}
+
+		final ShinyWriter writer = Shiny.writer();
+
+		Shiny.table()
+				.title("files of " + path)
+				.noDataFound("no files found")
+				.header("name", "isFile")
+				.rows(rows)
+				.render(writer);
 	}
 }
