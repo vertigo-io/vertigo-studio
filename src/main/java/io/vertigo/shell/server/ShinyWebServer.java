22c2d3658c4960d84ce305457240df91cc7b565d
phlppchrtn <philippe.chretien@kleegroup.com>
2025-09-16 18:34:45 +0200

    json

--- src/main/java/io/vertigo/shell/server/ShinyWebServer.java
+++ src/main/java/io/vertigo/shell/server/ShinyWebServer.java
@@ -1,14 +1,11 @@
 package io.vertigo.shell.server;
 
 import java.net.InetSocketAddress;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
@@ -19,7 +16,6 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.vertigo.shell.Shell;
-import io.vertigo.shell.systems.file.FileContext;
 import io.vertigo.shiny.Shiny;
 
 public class ShinyWebServer extends WebSocketServer {
@@ -63,38 +59,33 @@ public class ShinyWebServer extends WebSocketServer {
 				String userInput = wsMessage.data();
 				System.out.println("Prompt reçu : " + userInput);
 
-				if ("test".equals(userInput)) {
-					sendTable(conn);
-				} else if ("ls".equals(userInput)) {
-					final FileContext fileContext = FileContext.get();
-					final Path path = fileContext.getCurrentAbsolutePath();
-
+				if ("x-table".equals(userInput)) {
 					final List<String[]> rows = new ArrayList<>();
-
-					try (Stream<Path> stream = Files.list(path)) {
-						stream.forEach(p -> {
-							final String[] row = new String[2];
-							row[0] = p.getFileName().toString();
-							row[1] = Boolean.valueOf(!Files.isDirectory(p)).toString();
-							rows.add(row);
-						});
-						//					} catch (final IOException e) {
-						//						System.out.println("Error listing files: " + e.getMessage());
-					}
+					rows.add(new String[] { "Arthur", "Penn" });
+					rows.add(new String[] { "Marilyn", "Pinson" });
 					var table = Shiny.table()
-							.title("files of " + path)
+							.title("carnet d'adresses")
 							.noDataFound("no files found")
-							.header("name", "isFile")
+							.header("Prénom", "Nom")
 							.rows(rows);
 					mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
 					final String data = mapper.writeValueAsString(table);
-					final String json = String.format("""
+					sendMessage(conn, "table", data);
+				} else if ("x-json".equals(userInput)) {
+					var jsonContent = """
 							{
-							"type": "table", "data": %s
+							  "title": "The Shining",
+							  "director": "Stanley Kubrick",
+							  "release_year": 1980,
+							  "genre": ["Horror", "Thriller"],
+							  "duration": "2h 26m",
+							  "cast": ["Jack Nicholson", "Shelley Duvall", "Danny Lloyd"],
+							  "synopsis": "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence."
 							}
-							""", data);
-					System.out.println("send : " + json);
-					conn.send(json);
+															""";
+					var jsonData = new JsonData("titre ", "json vide", jsonContent);
+					final String data = mapper.writeValueAsString(jsonData);
+					sendMessage(conn, "json", data);
 				}
 
 			}
@@ -106,25 +97,15 @@ public class ShinyWebServer extends WebSocketServer {
 		}
 	}
 
-	private void sendTable(WebSocket conn) {
-		final String[] header = { "Nom", "Valeur" };
-		final List<String[]> rows = new ArrayList<>();
-		rows.add(new String[] { "Item1", "100" });
-		rows.add(new String[] { "Item2", "200" });
-		final TableData table = new TableData("Résultats", "Rien à afficher", header, rows);
-
-		try {
-			final String data = mapper.writeValueAsString(table);
-			final String json = String.format("""
-					{
-					"type": "table", "data": %s
-					}
-					""", data);
-			System.out.println("send : " + json);
-			conn.send(json);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private void sendMessage(WebSocket conn, String type, String data) {
+		final String json = String.format("""
+				{
+				"type": "%s",
+				"data": %s
+				}
+				""", type, data);
+		System.out.println("send : " + json);
+		conn.send(json);
 	}
 
 	@Override
@@ -148,12 +129,19 @@ public class ShinyWebServer extends WebSocketServer {
 	}
 
 	// Supposons que cette classe représente ta table
-	public static record TableData(
-			String tableTitle,
+	public static record JsonData(
+			String title,
 			String noDataFound,
-			String[] tableHeader,
-			List<String[]> tableRows) {
+			String json) {
 	}
+
+	//	// Supposons que cette classe représente ta table
+	//	public static record TableData(
+	//			String tableTitle,
+	//			String noDataFound,
+	//			String[] tableHeader,
+	//			List<String[]> tableRows) {
+	//	}
 
 	public static void main(String[] args) {
 		int port = 8080; // default port
