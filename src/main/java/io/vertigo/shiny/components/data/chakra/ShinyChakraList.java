e78bccbaf26682583d27cbbb5c5fc0d4f6f52783
phlppchrtn <philippe.chretien@kleegroup.com>
2025-09-25 09:53:30 +0200

    Pas de spé dans Shiny

--- src/main/java/io/vertigo/shiny/components/data/chakra/ShinyChakraList.java
+++
@@ -1,6 +0,0 @@
-package io.vertigo.shiny.components.data.chakra;
-
-import io.vertigo.shiny.components.ShinyComponent;
-
-public record ShinyChakraList(String title, String[] items) implements ShinyComponent {
-}
