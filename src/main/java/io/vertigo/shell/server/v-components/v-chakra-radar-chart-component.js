41f733c1e6142eff9b54c19090d0313361410cee
phlppchrtn <philippe.chretien@kleegroup.com>
2025-09-30 16:23:11 +0200

    fix

--- src/main/java/io/vertigo/shell/server/v-components/v-chakra-radar-chart-component.js
+++ src/main/java/io/vertigo/shell/server/v-components/v-chakra-radar-chart-component.js
@@ -6,14 +6,56 @@ Vue.component('v-chakra-radar-chart-component', {
         <canvas :id="canvasId"></canvas>
     </div>
     `,
-    data() {
-        return {
-            canvasId: `chakra-radarchart-${Math.random().toString(36).substr(2, 9)}`
-        };
-    },
-    mounted() {
-        const component = new ChakraRadarChartComponent(this.data);
-        component.canvasId = this.canvasId; // Ensure the canvasId is consistent
-        component.activate();
+	data() {
+	    return {
+	        canvasId: `chakra-radarchart-${Math.random().toString(36).substr(2, 9)}`
+	    };
+	},
+	mounted() {
+	    const ctx = document.getElementById(this.canvasId).getContext('2d');
+        const datasets = this.data.series.map((serie, index) => {
+            const color = ['#3182CE', '#63B3ED', '#4299E1', '#319795', '#81E6D9'][index % 5];
+            return {
+                label: serie.name,
+                data: serie.data,
+                backgroundColor: color + '80', // Add alpha transparency
+                borderColor: color,
+                borderWidth: 1
+            };
+        });
+
+        new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels: this.data.labels,
+                datasets: datasets
+            },
+            options: {
+                scales: {
+                    r: {
+                        angleLines: {
+                            color: '#4A5568'
+                        },
+                        grid: {
+                            color: '#4A5568'
+                        },
+                        pointLabels: {
+                            color: '#A0AEC0'
+                        },
+                        ticks: {
+                            color: '#A0AEC0',
+                            backdropColor: '#1A202C'
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: '#A0AEC0'
+                        }
+                    }
+                }
+            }
+        });
     }
 });
