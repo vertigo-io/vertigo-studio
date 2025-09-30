96f924aeb0c8d57afa906c98605de0f9a87aa094
phlppchrtn <philippe.chretien@kleegroup.com>
2025-09-30 18:52:44 +0200

    dark Form

--- src/main/java/io/vertigo/shell/server/v-components/v-form-component.js
+++ src/main/java/io/vertigo/shell/server/v-components/v-form-component.js
@@ -27,58 +27,83 @@ Vue.component('v-form-component', {
         }
     },
     template: `
-    <v-card class="mx-auto" max-width="800" style="margin-top: 1em;">
-        <v-card-title class="headline">{{ data.title }}</v-card-title>
+    <v-card class="form-card" max-width="800" dark>
+        <v-card-title class="text-h5 font-weight-bold primary--text">{{ data.title }}</v-card-title>
         <v-card-text>
-            <div v-for="(section, sectionIndex) in data.sections" :key="sectionIndex">
-                <h4 class="mt-4 mb-2">{{ section.title }}</h4>
-                <v-row>
-                    <v-col cols="12" sm="6" md="4" v-for="(field, fieldIndex) in section.fields" :key="fieldIndex">
-                        <template v-if="field.type === 'STRING'">
-                            <v-text-field
-                                :label="field.label"
-                                :value="field.value"
-                                readonly
-                                outlined
-                            ></v-text-field>
-                        </template>
-                        <template v-else-if="field.type === 'NUMBER'">
-                            <v-text-field
-                                :label="field.label"
-                                :value="field.value"
-                                readonly
-                                outlined
-                                type="number"
-                            ></v-text-field>
-                        </template>
-                        <template v-else-if="field.type === 'DATE'">
-                            <v-text-field
-                                :label="field.label"
-                                :value="field.value"
-                                readonly
-                                outlined
-                            ></v-text-field>
-                        </template>
-                        <template v-else-if="field.type === 'BOOLEAN'">
-                            <v-checkbox
-                                :label="field.label"
-                                :input-value="field.value"
-                                readonly
-                            ></v-checkbox>
-                        </template>
-                        <template v-else-if="field.type === 'IMAGE'">
-                            <v-img
-                                :src="field.value"
-                                :alt="field.label"
-                                contain
-                                max-height="150"
-                            ></v-img>
-                            <div class="text-caption">{{ field.label }}</div>
-                        </template>
-                    </v-col>
-                </v-row>
-            </div>
+            <v-expansion-panels flat multiple>
+                <v-expansion-panel v-for="(section, sectionIndex) in data.sections" :key="sectionIndex" class="form-section-panel mb-4">
+                    <v-expansion-panel-header class="text-h6 font-weight-medium white--text">{{ section.title }}</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <v-row dense>
+                            <v-col cols="12" sm="6" md="4" v-for="(field, fieldIndex) in section.fields" :key="fieldIndex">
+                                <template v-if="field.type === 'STRING'">
+                                    <v-text-field
+                                        :label="field.label"
+                                        :value="field.value"
+                                        readonly
+                                        filled
+                                        dark
+                                        dense
+                                        color="primary"
+                                    ></v-text-field>
+                                </template>
+                                <template v-else-if="field.type === 'NUMBER'">
+                                    <v-text-field
+                                        :label="field.label"
+                                        :value="field.value"
+                                        readonly
+                                        filled
+                                        dark
+                                        dense
+                                        type="number"
+                                        color="primary"
+                                    ></v-text-field>
+                                </template>
+                                <template v-else-if="field.type === 'DATE'">
+                                    <v-text-field
+                                        :label="field.label"
+                                        :value="field.value"
+                                        readonly
+                                        filled
+                                        dark
+                                        dense
+                                        color="primary"
+                                    ></v-text-field>
+                                </template>
+                                <template v-else-if="field.type === 'BOOLEAN'">
+                                    <v-switch
+                                        :label="field.label"
+                                        :input-value="field.value"
+                                        readonly
+                                        inset
+                                        color="primary"
+                                    ></v-switch>
+                                </template>
+                                <template v-else-if="field.type === 'IMAGE'">
+                                    <v-img
+                                        :src="field.value"
+                                        :alt="field.label"
+                                        contain
+                                        max-height="150"
+                                        class="mb-2"
+                                    ></v-img>
+                                    <div class="text-caption white--text">{{ field.label }}</div>
+                                </template>
+                            </v-col>
+                        </v-row>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
         </v-card-text>
     </v-card>
+    <style scoped>
+    .form-card {
+        margin-top: 1em;
+        background-color: #2D3748 !important; /* Override Vuetify default */
+    }
+    .form-section-panel {
+        background-color: #1A202C !important; /* Override Vuetify default */
+    }
+    </style>
     `,
 });
