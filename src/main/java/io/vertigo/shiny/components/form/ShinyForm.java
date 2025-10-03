6ff94fda2fb68ed774af8fe7b65df4a4528e5356
phlppchrtn <philippe.chretien@kleegroup.com>
2025-10-03 22:41:59 +0200

    fix

--- src/main/java/io/vertigo/shiny/components/form/ShinyForm.java
+++ src/main/java/io/vertigo/shiny/components/form/ShinyForm.java
@@ -3,8 +3,10 @@ package io.vertigo.shiny.components.form;
 import java.util.List;
 
 import io.vertigo.core.lang.Assertion;
+import io.vertigo.shiny.ShinyType;
 import io.vertigo.shiny.components.ShinyComponent;
 
+@ShinyType("form")
 public record ShinyForm(String title, List<ShinyFormSection> sections) implements ShinyComponent {
 	public ShinyForm {
 		Assertion.check()
