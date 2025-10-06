855d6054535be8aeab74ffcc34b5b245d80d697d
phlppchrtn <philippe.chretien@kleegroup.com>
2025-10-06 10:47:31 +0200

    var private

--- src/main/java/io/vertigo/shiny/components/data/list/ShinyList.java
+++ src/main/java/io/vertigo/shiny/components/data/list/ShinyList.java
@@ -2,13 +2,19 @@ package io.vertigo.shiny.components.data.list;
 
 import java.util.List;
 
-import io.vertigo.shiny.components.ShinyComponent;
-
+import io.vertigo.core.lang.Assertion;
 import io.vertigo.shiny.ShinyType;
+import io.vertigo.shiny.components.ShinyComponent;
 
 @ShinyType("list")
 public record ShinyList(
 		String title,
 		List<Object> items, //String || ShinyList
 		ShinyListType type) implements ShinyComponent {
+
+	public ShinyList {
+		Assertion.check()
+				.isNotNull(items)
+				.isNotNull(type);
+	}
 }
--- src/main/java/io/vertigo/shiny/components/data/table/ShinyTableBuilder.java
+++ src/main/java/io/vertigo/shiny/components/data/table/ShinyTableBuilder.java
@@ -8,79 +8,79 @@ import io.vertigo.core.lang.Assertion;
 import io.vertigo.core.lang.Builder;
 
 public final class ShinyTableBuilder implements Builder<ShinyTable> {
-	private String myId;
-	private String myTitle;
-	private String myNoDataFound;
-	private String[] myHeader;
-	private final List<String[]> myRows = new ArrayList<>();
-	private boolean mySortable;
-	private int mySortColumn = -1;
-	private String mySortDirection = "asc"; // "asc" or "desc"
+	private String _id;
+	private String _title;
+	private String _noDataFound;
+	private String[] _header;
+	private final List<String[]> _rows = new ArrayList<>();
+	private boolean _sortable;
+	private int _sortColumn = -1;
+	private String _sortDirection = "asc"; // "asc" or "desc"
 
 	public ShinyTableBuilder() {
-		myId = UUID.randomUUID().toString();
+		_id = UUID.randomUUID().toString();
 	}
 
 	public ShinyTableBuilder withId(final String id) {
 		Assertion.check().isNotBlank(id);
 		//---
-		myId = id;
+		this._id = id;
 		return this;
 	}
 
 	public ShinyTableBuilder withTitle(final String title) {
 		Assertion.check().isNotBlank(title);
 		//---
-		myTitle = title;
+		this._title = title;
 		return this;
 	}
 
 	public ShinyTableBuilder withNoDataFound(final String noDataFound) {
 		Assertion.check().isNotBlank(noDataFound);
 		//---
-		myNoDataFound = noDataFound;
+		this._noDataFound = noDataFound;
 		return this;
 	}
 
 	public ShinyTableBuilder withHeader(final String... header) {
 		Assertion.check().isNotNull(header);
 		//---
-		myHeader = header;
+		this._header = header;
 		return this;
 	}
 
 	public ShinyTableBuilder addRow(final String... row) {
 		Assertion.check().isNotNull(row);
 		//---
-		myRows.add(row);
+		this._rows.add(row);
 		return this;
 	}
 
 	public ShinyTableBuilder addAllRows(final List<String[]> rows) {
 		Assertion.check().isNotNull(rows);
 		//---
-		myRows.addAll(rows);
+		this._rows.addAll(rows);
 		return this;
 	}
 
 	public ShinyTableBuilder withSortable(final boolean sortable) {
-		mySortable = sortable;
+		this._sortable = sortable;
 		return this;
 	}
 
 	public ShinyTableBuilder withSortColumn(final int sortColumn) {
-		mySortColumn = sortColumn;
+		this._sortColumn = sortColumn;
 		return this;
 	}
 
 	public ShinyTableBuilder withSortDirection(final String sortDirection) {
 		Assertion.check().isNotBlank(sortDirection);
 		//---
-		mySortDirection = sortDirection;
+		this._sortDirection = sortDirection;
 		return this;
 	}
 
 	public ShinyTable build() {
-		return new ShinyTable(myId, myTitle, myNoDataFound, myHeader, myRows.toArray(String[][]::new), mySortable, mySortColumn, mySortDirection);
+		return new ShinyTable(_id, _title, _noDataFound, _header, _rows.toArray(String[][]::new), _sortable, _sortColumn, _sortDirection);
 	}
 }
