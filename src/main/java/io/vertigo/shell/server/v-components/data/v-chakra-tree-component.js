b3af90e960646df4deb0c269a27a44329e4d2202
phlppchrtn <philippe.chretien@kleegroup.com>
2025-10-07 18:13:07 +0200

    + simple

--- src/main/java/io/vertigo/shell/server/v-components/data/v-chakra-tree-component.js
+++ src/main/java/io/vertigo/shell/server/v-components/data/v-chakra-tree-component.js
@@ -2,20 +2,13 @@ Vue.component('v-chakra-tree-component', {
   props: ['data'],
   template: `
     <div class="chakra-tree">
-      <node :node="data"></node>
-    </div>
-  `
-});
-
-Vue.component('node', {
-  props: ['node'],
-  template: `
-    <div class="chakra-tree-node">
-      <i v-if="node.leaf" data-lucide="file" class="chakra-tree-node-icon"></i>
-      <i v-else data-lucide="folder" class="chakra-tree-node-icon"></i>
-      <span class="chakra-tree-node-label">{{ node.label }}</span>
-      <div v-if="node.children && node.children.length > 0" class="chakra-tree-node-children">
-        <node v-for="(child, index) in node.children" :key="index" :node="child"></node>
+      <div class="chakra-tree-node">
+        <i v-if="data.leaf" data-lucide="file" class="chakra-tree-node-icon"></i>
+        <i v-else data-lucide="folder" class="chakra-tree-node-icon"></i>
+        <span class="chakra-tree-node-label">{{ data.label }}</span>
+        <div v-if="data.children && data.children.length > 0" class="chakra-tree-node-children">
+          <v-chakra-tree-component v-for="(child, index) in data.children" :key="index" :data="child"></v-chakra-tree-component>
+        </div>
       </div>
     </div>
   `
