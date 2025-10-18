3f85f6098ad6a55b6c86916a0175991da48f4e64
phlppchrtn <philippe.chretien@kleegroup.com>
2025-10-18 14:11:29 +0200

    fix

--- src/main/java/io/vertigo/shiny/models/ShinyProp.java
+++ src/main/java/io/vertigo/shiny/models/ShinyProp.java
@@ -1,12 +1,12 @@
 package io.vertigo.shiny.models;
 
-public record ShinyProp(String name, String value) {
+public record ShinyProp(String key, String value) {
 
-	public static ShinyProp of(String name, String value) {
-		return new ShinyProp(name, value);
+	public static ShinyProp of(String key, String value) {
+		return new ShinyProp(key, value);
 	}
 
-	public static ShinyProp of(String name, int value) {
-		return new ShinyProp(name, String.valueOf(value));
+	public static ShinyProp of(String key, int value) {
+		return new ShinyProp(key, String.valueOf(value));
 	}
 }
