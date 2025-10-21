eca3de38493c4c47db60680f953a59da002c167a
phlppchrtn <philippe.chretien@kleegroup.com>
2025-10-21 18:27:31 +0200

    chip (data -> text)

--- banshee/src/components/text/v-shiny-chip-component.vue
+++ banshee/src/components/text/v-shiny-chip-component.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script setup lang="ts">
-import type { ShinyChip } from '@/models/data/chip/ShinyChip';
+import type { ShinyChip } from '@/models/text/chip/ShinyChip';
 
 defineProps<{
   data: ShinyChip
