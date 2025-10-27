b774fe2443be1eede7cb5f2122103c7f754221a3
phlppchrtn <philippe.chretien@kleegroup.com>
2025-10-27 10:14:26 +0100

    fix

--- src/main/java/io/vertigo/banshee/com/BansheeCommand.java
+++ src/main/java/io/vertigo/banshee/com/BansheeCommand.java
@@ -7,6 +7,6 @@ import io.vertigo.shiny.models.ShinyProp;
 
 public record BansheeCommand(
 		String command,
-		UUID id, //facultatif id d'un model
+		String id, //facultatif id d'un model
 		List<ShinyProp> props) {
 }
