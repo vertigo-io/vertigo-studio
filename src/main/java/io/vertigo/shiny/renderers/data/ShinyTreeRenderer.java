06c237c84cde1bf588d6c576bef8eebbf025c4d8
phlppchrtn <philippe.chretien@kleegroup.com>
2025-11-05 23:13:49 +0100

    fix

--- src/main/java/io/vertigo/shiny/renderers/data/ShinyTreeRenderer.java
+++ src/main/java/io/vertigo/shiny/renderers/data/ShinyTreeRenderer.java
@@ -7,7 +7,6 @@ import io.vertigo.shiny.Shiny;
 import io.vertigo.shiny.ShinyWriter;
 import io.vertigo.shiny.models.ShinyModel;
 import io.vertigo.shiny.models.data.tree.ShinyTree;
-import io.vertigo.shiny.models.data.tree.ShinyTreeNode;
 import io.vertigo.shiny.renderers.ShinyModelRenderer;
 
 public final class ShinyTreeRenderer implements ShinyModelRenderer<ShinyTree> {
@@ -21,21 +20,19 @@ public final class ShinyTreeRenderer implements ShinyModelRenderer<ShinyTree> {
 		Assertion.check()
 				.isNotNull(shinyTree);
 		//---
-		//		final ShinyTreeStyle style = Shiny.theme().treeStyle();
 		final ShinyWriter writer = Shiny.writer();
-
-		writer.println(shinyTree.rootNode().getIcon().getValue() + " " + shinyTree.rootNode().getLabel());
-		printChildren(writer, "", shinyTree.rootNode().getNodes());
+		writer.println(shinyTree.label());
+		printChildren(writer, "", shinyTree.children());
 	}
 
-	private static void printChildren(final ShinyWriter writer, final String prefix, final List<ShinyTreeNode> children) {
+	private static void printChildren(final ShinyWriter writer, final String prefix, final List<ShinyTree> children) {
 		for (int i = 0; i < children.size(); i++) {
 			final boolean isLast = (i == children.size() - 1);
-			final String connection = (isLast ? ShinyTreeChars.BOTTOM_LEFT : ShinyTreeChars.INNER_LEFT) + ShinyTreeChars.HORIZONTAL + ShinyTreeChars.HORIZONTAL;
-			final ShinyTreeNode child = children.get(i);
-			writer.println(prefix + connection + child.getIcon().getValue() + " " + child.getLabel());
-			final List<ShinyTreeNode> grandChildren = child.getNodes();
-			final String childPrefix = prefix + (isLast ? "   " : ShinyTreeChars.VERTICAL + "  ");
+			final String connection = (isLast ? "└──" : "├──") + "──";
+			final ShinyTree child = children.get(i);
+			writer.println(prefix + connection + child.label());
+			final List<ShinyTree> grandChildren = child.children();
+			final String childPrefix = prefix + (isLast ? "   " : "│  ");
 			printChildren(writer, childPrefix, grandChildren);
 		}
 	}
