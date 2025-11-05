c99bf8a2d9963720a9cc86c82c599248d4c5bd6e
phlppchrtn <philippe.chretien@kleegroup.com>
2025-11-05 23:23:16 +0100

    fix

--- src/main/java/io/vertigo/shiny/renderers/text/ShinyMarkDownRenderer.java
+++ src/main/java/io/vertigo/shiny/renderers/text/ShinyMarkDownRenderer.java
@@ -7,28 +7,26 @@ import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 
 import io.vertigo.core.lang.Assertion;
-import io.vertigo.shiny.ShinyRenderer; // New import
-import io.vertigo.shiny.ShinyWriter;
-import io.vertigo.shiny.models.ShinyComponent;
+import io.vertigo.shiny.models.ShinyModel;
 import io.vertigo.shiny.models.text.markdown.ShinyMarkDown;
+import io.vertigo.shiny.renderers.ShinyModelRenderer;
 
-public final class ShinyMarkDownRenderer implements ShinyRenderer<ShinyMarkDown> {
+public final class ShinyMarkDownRenderer implements ShinyModelRenderer<ShinyMarkDown> {
 
-	@Override 
-	public boolean accept(final ShinyComponent component) {
-		return component instanceof ShinyMarkDown;
+	@Override
+	public boolean accept(final ShinyModel model) {
+		return model instanceof ShinyMarkDown;
 	}
 
-	@Override 
-	public void render(final ShinyMarkDown shinyMarkDown, final ShinyWriter writer) { 
-		Assertion.check().isNotNull(shinyMarkDown);
-		Assertion.check().isNotNull(writer);
-		Assertion.check().isNotNull(shinyMarkDown.markdownText(), "Markdown text not set. Use fromFile() or fromText()."); // Access directly
+	@Override
+	public void render(final ShinyMarkDown shinyMarkDown) {
+		Assertion.check()
+				.isNotNull(shinyMarkDown);
 		//---
 		final Parser parser = Parser.builder()
 				.extensions(Collections.singletonList(TablesExtension.create()))
 				.build();
 		final Node document = parser.parse(shinyMarkDown.markdownText()); // Access directly
-		document.accept(new ShinyMarkdownVisitor(writer));
+		document.accept(new ShinyMarkdownVisitor());
 	}
 }
--- src/main/java/io/vertigo/shiny/renderers/text/ShinyMarkdownVisitor.java
+++ src/main/java/io/vertigo/shiny/renderers/text/ShinyMarkdownVisitor.java
@@ -18,88 +18,91 @@ import org.commonmark.node.Paragraph;
 import org.commonmark.renderer.text.TextContentRenderer;
 
 import io.vertigo.shiny.Shiny;
-import io.vertigo.shiny.ShinyWriter;
+import io.vertigo.shiny.models.data.list.ShinyListType;
 
 class ShinyMarkdownVisitor extends AbstractVisitor {
-    private final TextContentRenderer textContentRenderer = TextContentRenderer.builder().build();
-    private final ShinyWriter writer;
+	private final TextContentRenderer textContentRenderer = TextContentRenderer.builder().build();
 
-    public ShinyMarkdownVisitor(final ShinyWriter writer) {
-        this.writer = writer;
-    }
+	@Override
+	public void visit(final Heading heading) {
+		final String title = textContentRenderer.render(heading);
+		Shiny.render(
+				Shiny.title()
+						.withLevel(heading.getLevel())
+						.withText(title)
+						.build());
+	}
 
-    @Override
-    public void visit(final Heading heading) {
-        final String title = textContentRenderer.render(heading);
-        Shiny.title().level(heading.getLevel()).text(title).render(writer);
-    }
+	@Override
+	public void visit(final Paragraph paragraph) {
+		final String text = textContentRenderer.render(paragraph);
+		Shiny.render(
+				Shiny.paragraph().withText(text).build());
+	}
 
-    @Override
-    public void visit(final Paragraph paragraph) {
-        final String text = textContentRenderer.render(paragraph);
-        Shiny.paragraph().text(text).render(writer);
-    }
+	@Override
+	public void visit(final BulletList bulletList) {
+		final List<String> items = new ArrayList<>();
+		for (Node child = bulletList.getFirstChild(); child != null; child = child.getNext()) {
+			if (child instanceof ListItem) {
+				items.add(textContentRenderer.render(child));
+			}
+		}
+		Shiny.render(
+				Shiny
+						.list()
+						.addAllItems(items)
+						.build());
+	}
 
-    @Override
-    public void visit(final BulletList bulletList) {
-        final List<String> items = new ArrayList<>();
-        for (Node child = bulletList.getFirstChild(); child != null; child = child.getNext()) {
-            if (child instanceof ListItem) {
-                items.add(textContentRenderer.render(child));
-            }
-        }
-        Shiny
-                .list()
-                .items(items)
-                .render(writer);
-    }
+	@Override
+	public void visit(final OrderedList orderedList) {
+		final List<String> items = new ArrayList<>();
+		for (Node child = orderedList.getFirstChild(); child != null; child = child.getNext()) {
+			if (child instanceof ListItem) {
+				items.add(textContentRenderer.render(child));
+			}
+		}
+		Shiny.render(
+				Shiny.list()
+						.withType(ShinyListType.ORDERED)
+						.addAllItems(items)
+						.build());
+	}
 
-    @Override
-    public void visit(final OrderedList orderedList) {
-        final List<String> items = new ArrayList<>();
-        for (Node child = orderedList.getFirstChild(); child != null; child = child.getNext()) {
-            if (child instanceof ListItem) {
-                items.add(textContentRenderer.render(child));
-            }
-        }
-        Shiny.list()
-                .ordered()
-                .items(items)
-                .render(writer);
-    }
+	@Override
+	public void visit(final CustomBlock tableBlock) {
+		final List<String> headers = new ArrayList<>();
+		final List<String[]> rows = new ArrayList<>();
 
-    @Override
-    public void visit(final CustomBlock tableBlock) {
-        final List<String> headers = new ArrayList<>();
-        final List<String[]> rows = new ArrayList<>();
+		final TableHead tableHead = (TableHead) tableBlock.getFirstChild();
+		if (tableHead != null) {
+			final TableRow headerRow = (TableRow) tableHead.getFirstChild();
+			for (Node child = headerRow.getFirstChild(); child != null; child = child.getNext()) {
+				if (child instanceof TableCell) {
+					headers.add(textContentRenderer.render(child));
+				}
+			}
+		}
 
-        final TableHead tableHead = (TableHead) tableBlock.getFirstChild();
-        if (tableHead != null) {
-            final TableRow headerRow = (TableRow) tableHead.getFirstChild();
-            for (Node child = headerRow.getFirstChild(); child != null; child = child.getNext()) {
-                if (child instanceof TableCell) {
-                    headers.add(textContentRenderer.render(child));
-                }
-            }
-        }
-
-        final TableBody tableBody = (TableBody) tableHead.getNext();
-        if (tableBody != null) {
-            for (Node rowNode = tableBody.getFirstChild(); rowNode != null; rowNode = rowNode.getNext()) {
-                if (rowNode instanceof TableRow) {
-                    final List<String> cells = new ArrayList<>();
-                    for (Node cellNode = rowNode.getFirstChild(); cellNode != null; cellNode = cellNode.getNext()) {
-                        if (cellNode instanceof TableCell) {
-                            cells.add(textContentRenderer.render(cellNode));
-                        }
-                    }
-                    rows.add(cells.toArray(new String[0]));
-                }
-            }
-        }
-        Shiny.table()
-                .header(headers.toArray(new String[0]))
-                .rows(rows)
-                .render(writer);
-    }
+		final TableBody tableBody = (TableBody) tableHead.getNext();
+		if (tableBody != null) {
+			for (Node rowNode = tableBody.getFirstChild(); rowNode != null; rowNode = rowNode.getNext()) {
+				if (rowNode instanceof TableRow) {
+					final List<String> cells = new ArrayList<>();
+					for (Node cellNode = rowNode.getFirstChild(); cellNode != null; cellNode = cellNode.getNext()) {
+						if (cellNode instanceof TableCell) {
+							cells.add(textContentRenderer.render(cellNode));
+						}
+					}
+					rows.add(cells.toArray(new String[0]));
+				}
+			}
+		}
+		Shiny.render(
+				Shiny.table()
+						.withHeader(headers.toArray(new String[0]))
+						.addAllRows(rows)
+						.build());
+	}
 }
