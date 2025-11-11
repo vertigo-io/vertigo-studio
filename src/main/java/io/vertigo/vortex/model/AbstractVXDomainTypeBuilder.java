f10cb2767f6a78cacb1177fc0f8149359b1f5319
phlppchrtn <philippe.chretien@kleegroup.com>
2025-11-11 23:38:57 +0100

    fix

--- src/main/java/io/vertigo/vortex/model/AbstractVXDomainTypeBuilder.java
+++ src/main/java/io/vertigo/vortex/model/AbstractVXDomainTypeBuilder.java
@@ -6,21 +6,21 @@ import java.util.List;
 import io.vertigo.core.lang.Builder;
 
 public abstract class AbstractVXDomainTypeBuilder<B extends AbstractVXDomainTypeBuilder<B>> implements Builder<VXDomainType> {
-    protected final String _name;
-    protected final VXDataType _dataType;
-    protected final List<VXValidator> _validators = new ArrayList<>();
+	protected final String _name;
+	protected final VXDataType _dataType;
+	protected final List<VXValidator> _validators = new ArrayList<>();
 
-    protected AbstractVXDomainTypeBuilder(final String name, final VXDataType dataType) {
-        _name = name;
-        _dataType = dataType;
-    }
+	protected AbstractVXDomainTypeBuilder(final String name, final VXDataType dataType) {
+		_name = name;
+		_dataType = dataType;
+	}
 
-    protected B self() {
-        return (B) this;
-    }
+	protected final B self() {
+		return (B) this;
+	}
 
-    @Override
-    public VXDomainType build() {
-        return new VXDomainType(_name, _dataType.name(), _validators);
-    }
+	@Override
+	public final VXDomainType build() {
+		return new VXDomainType(_name, _dataType.name(), _validators);
+	}
 }
--- src/test/java/io/vertigo/shiny/models/text/markdown/ShinyMarkDownTest.java
+++ src/test/java/io/vertigo/shiny/models/text/markdown/ShinyMarkDownTest.java
@@ -2,20 +2,17 @@ package io.vertigo.shiny.models.text.markdown;
 
 import java.io.IOException;
 
-import io.vertigo.shiny.Shiny;
-import io.vertigo.shiny.ShinyWriter;
-
 public class ShinyMarkDownTest {
 
-    public static void main(final String[] args) throws IOException {
-        final ShinyWriter writer = Shiny.writer();
-        System.out.println("--- Testing Title ---");
-        Shiny.markdown().builder().withText("# Title 1").build().render(writer);
-        System.out.println("--- Testing List ---");
-        Shiny.markdown().builder().withText("* item 1\n* item 2").build().render(writer);
-        System.out.println("--- Testing Table ---");
-        Shiny.markdown().builder().withText("| h1 | h2 |\n|---|---|\n| c1 | c2 |").build().render(writer);
-        System.out.println("--- Testing Paragraph ---");
-        Shiny.markdown().builder().withText("This is a paragraph.").build().render(writer);
-    }
+	public static void main(final String[] args) throws IOException {
+		//        final ShinyWriter writer = Shiny.writer();
+		//        System.out.println("--- Testing Title ---");
+		//        Shiny.markdown().builder().withText("# Title 1").build().render(writer);
+		//        System.out.println("--- Testing List ---");
+		//        Shiny.markdown().builder().withText("* item 1\n* item 2").build().render(writer);
+		//        System.out.println("--- Testing Table ---");
+		//        Shiny.markdown().builder().withText("| h1 | h2 |\n|---|---|\n| c1 | c2 |").build().render(writer);
+		//        System.out.println("--- Testing Paragraph ---");
+		//        Shiny.markdown().builder().withText("This is a paragraph.").build().render(writer);
+	}
 }
