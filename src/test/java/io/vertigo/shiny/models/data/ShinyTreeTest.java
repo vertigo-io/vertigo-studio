6beedf29091b9da50101abafe3cdb28add607c75
phlppchrtn <philippe.chretien@kleegroup.com>
2025-11-11 20:57:17 +0100

    fix

--- src/test/java/io/vertigo/shiny/models/data/ShinyTreeTest.java
+++ src/test/java/io/vertigo/shiny/models/data/ShinyTreeTest.java
@@ -1,102 +1,27 @@
 package io.vertigo.shiny.models.data;
 
-import static io.vertigo.shiny.renderers.ShinyIcon.ARROW_DOWN;
-import static io.vertigo.shiny.renderers.ShinyIcon.ARROW_LEFT;
-import static io.vertigo.shiny.renderers.ShinyIcon.ARROW_RIGHT;
-import static io.vertigo.shiny.renderers.ShinyIcon.ARROW_UP;
-import static io.vertigo.shiny.renderers.ShinyIcon.CLOCK;
-import static io.vertigo.shiny.renderers.ShinyIcon.DB;
-import static io.vertigo.shiny.renderers.ShinyIcon.ERROR;
-import static io.vertigo.shiny.renderers.ShinyIcon.FILE;
-import static io.vertigo.shiny.renderers.ShinyIcon.FOLDER_CLOSED;
-import static io.vertigo.shiny.renderers.ShinyIcon.FOLDER_OPEN;
-import static io.vertigo.shiny.renderers.ShinyIcon.HEART;
-import static io.vertigo.shiny.renderers.ShinyIcon.INFO;
-import static io.vertigo.shiny.renderers.ShinyIcon.QUESTION;
-import static io.vertigo.shiny.renderers.ShinyIcon.SMILEY;
-import static io.vertigo.shiny.renderers.ShinyIcon.STAR;
-import static io.vertigo.shiny.renderers.ShinyIcon.SUCCESS;
-import static io.vertigo.shiny.renderers.ShinyIcon.USER;
-import static io.vertigo.shiny.renderers.ShinyIcon.WARNING;
-
 import io.vertigo.shiny.Shiny;
-import io.vertigo.shiny.ShinyWriter;
 import io.vertigo.shiny.style.ShinyColors;
 
 public class ShinyTreeTest {
 	public static void main(final String[] args) {
-		final ShinyWriter writer = Shiny.writer();
-		testVideoGamesTree(writer);
-		testIconShowcase(writer);
-		testComplexTreeStructure(writer);
+		testVideoGamesTree();
 	}
 
-	private static void testVideoGamesTree(final ShinyWriter writer) {
-		writer.println(ShinyColors.BLUE_BRIGHT.fg("--- Video Games Tree ---"));
-		final var tree = Shiny.tree("Video Games").build();
-		tree.getRoot()
-				.addChild("RPG", FOLDER_OPEN)
-				.addChild("Final Fantasy", FILE).up()
-				.addChild("The Elder Scrolls", FILE);
-		tree.getRoot()
-				.addChild("FPS", FOLDER_OPEN)
-				.addChild("Halo", FILE).up()
-				.addChild("Call of Duty", FILE);
-		tree.getRoot()
-				.addChild("Adventure", FOLDER_OPEN)
-				.addChild("The Legend of Zelda", FILE).up()
-				.addChild("Uncharted", FILE);
+	private static void testVideoGamesTree() {
+		Shiny.writer().println(ShinyColors.BLUE_BRIGHT.fg("--- Video Games Tree ---"));
+		final var treeBuilder = Shiny.tree().withLabel("Video Games");
+		treeBuilder.addTree("RPG")
+				.addLeaf("Final Fantasy")
+				.addLeaf("The Elder Scrolls");
+		treeBuilder.addTree("FPS")
+				.addLeaf("Halo")
+				.addLeaf("Call of Duty");
+		treeBuilder.addTree("Adventure")
+				.addLeaf("The Legend of Zelda")
+				.addLeaf("Uncharted");
 
-		Shiny.render(tree);
-		writer.println();
-	}
-
-	private static void testIconShowcase(final ShinyWriter writer) {
-		writer.println(ShinyColors.BLUE_BRIGHT.fg("--- Icon Showcase ---"));
-		final var tree = Shiny.tree("Icons").build();
-		tree.getRoot()
-				.addChild("Status Icons", FOLDER_OPEN)
-				.addChild("Success", SUCCESS).up()
-				.addChild("Error", ERROR).up()
-				.addChild("Warning", WARNING).up()
-				.addChild("Info", INFO).up()
-				.addChild("Question", QUESTION).up();
-		tree.getRoot()
-				.addChild("Time & Arrows", FOLDER_OPEN)
-				.addChild("Clock", CLOCK).up()
-				.addChild("Up", ARROW_UP).up()
-				.addChild("Down", ARROW_DOWN).up()
-				.addChild("Left", ARROW_LEFT).up()
-				.addChild("Right", ARROW_RIGHT).up();
-		tree.getRoot()
-				.addChild("Misc Icons", FOLDER_OPEN)
-				.addChild("Star", STAR).up()
-				.addChild("Heart", HEART).up()
-				.addChild("Smiley", SMILEY).up();
-		Shiny.render(tree);
-		writer.println();
-	}
-
-	private static void testComplexTreeStructure(final ShinyWriter writer) {
-		writer.println(ShinyColors.BLUE_BRIGHT.fg("--- Complex Tree Structure ---"));
-		final var tree = Shiny.tree("Project Structure").build();
-		final var srcNode = tree.getRoot().addChild("src", FOLDER_CLOSED);
-		final var main = srcNode.addChild("main", FOLDER_CLOSED);
-		final var java = main.addChild("java", FOLDER_CLOSED);
-		java.addChild("com.example.app", FOLDER_CLOSED);
-		java.addChild("Application.java", FILE);
-		final var resources = srcNode.addChild("resources", FOLDER_CLOSED);
-		resources.addChild("application.properties", FILE);
-		final var testNode = tree.getRoot().addChild("test", FOLDER_CLOSED);
-		final var javaTest = testNode.addChild("java", FOLDER_CLOSED);
-		javaTest.addChild("com.example.app", FOLDER_CLOSED);
-		javaTest.addChild("ApplicationTest.java", FILE);
-
-		tree.getRoot().addChild("pom.xml", FILE);
-		tree.getRoot().addChild("database", DB);
-		tree.getRoot().addChild("admin", USER);
-
-		Shiny.render(tree);
-		writer.println();
+		Shiny.render(treeBuilder.build());
+		Shiny.writer().println();
 	}
 }
