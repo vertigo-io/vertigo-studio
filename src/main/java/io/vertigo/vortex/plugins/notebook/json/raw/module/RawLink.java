128c278169e419fddba349916a99634d5eb4ad6c
phlppchrtn <philippe.chretien@kleegroup.com>
2025-12-27 03:08:37 +0100

    fix

--- src/main/java/io/vertigo/vortex/plugins/notebook/json/raw/module/RawLink.java
+++ src/main/java/io/vertigo/vortex/plugins/notebook/json/raw/module/RawLink.java
@@ -3,10 +3,20 @@ package io.vertigo.vortex.plugins.notebook.json.raw.module;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.vertigo.core.lang.Assertion;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record RawLink(
 		String label,
 		String comment,
 		@JsonProperty("target-entity-key") String targetEntityKey,
 		String stereotype) {
+
+	public RawLink {
+		Assertion.check()
+				.isNotBlank(label)
+				//comment may be null
+				.isNotNull(targetEntityKey)
+				.isNotNull(stereotype);
+	}
 }
