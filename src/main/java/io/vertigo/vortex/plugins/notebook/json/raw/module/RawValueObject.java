1d49de6cfa90d0ba3837908b04f61896b991f106
phlppchrtn <philippe.chretien@kleegroup.com>
2026-01-07 10:35:39 +0100

    fix

--- src/main/java/io/vertigo/vortex/plugins/notebook/json/raw/module/RawValueObject.java
+++ src/main/java/io/vertigo/vortex/plugins/notebook/json/raw/module/RawValueObject.java
@@ -1,6 +1,5 @@
 package io.vertigo.vortex.plugins.notebook.json.raw.module;
 
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,29 +8,18 @@ import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.vertigo.core.lang.Assertion;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record RawValueObject(
 		@JsonProperty("_comment") String _comment,
 		Map<String, RawAttribute> attributes) {
 
 	public RawValueObject {
-		attributes = attributes != null
-				? Collections.unmodifiableMap(new LinkedHashMap<>(attributes))
-				: Map.of();
-
-		// Optionnel : interdiction des clés commençant par _
-		Assertion.check().isTrue(
-				attributes.keySet().stream().noneMatch(k -> k.startsWith("_")),
-				"Attribute keys must not start with '_' (reserved for internal use)");
+		attributes = new LinkedHashMap<>();
 	}
 
 	@JsonAnySetter
 	private void setAttribute(String key, RawAttribute value) {
-		if (attributes instanceof LinkedHashMap<String, RawAttribute> map) {
-			map.put(key, value);
-		}
+		attributes.put(key, value);
 	}
 
 	@JsonAnyGetter
