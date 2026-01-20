1d99e601a8282b8e3849bfb1a47bad5c9860ff9b
phlppchrtn <philippe.chretien@kleegroup.com>
2026-01-20 23:54:03 +0100

    commandLine

--- src/main/java/io/vertigo/banshee/com/BansheeCommand.java
+++ src/main/java/io/vertigo/banshee/com/BansheeCommand.java
@@ -5,10 +5,24 @@ import java.util.UUID;
 import io.vertigo.shiny.models.ShinyState;
 
 /**
- * Le client envoie une commande au serveur
+ * Le client envoie une commandeLine au serveur.
  */
 public record BansheeCommand(
-		String commandLine,
+		String commandLine, // command + args
 		UUID id, //facultatif id d'un model
 		ShinyState state) { //facultatif nouvel état d'affichage d'un modèle 
+
+	public String command() {
+		int i = commandLine.indexOf(" ");
+		return i > 0
+				? commandLine.substring(0, i)
+				: commandLine;
+	}
+
+	public String args() {
+		int i = commandLine.indexOf(" ");
+		return i > 0
+				? commandLine.substring(i + 1)
+				: "";
+	}
 }
