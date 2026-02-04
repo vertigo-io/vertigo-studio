914bf8918ea3d1d23eed2233859a1bdce1efdc44
phlppchrtn <philippe.chretien@kleegroup.com>
2026-02-04 23:05:27 +0100

    doc

--- banshee/src/models/data/table/cell/ShinyStringCell.ts
+++ banshee/src/models/data/table/cell/ShinyStringCell.ts
@@ -1,3 +1,7 @@
+// WARNING: DO NOT EDIT MANUALLY
+// This file is the TypeScript representation of a Java model.
+// Any changes must be made in the corresponding Java file first.
+
 import { ShinyTableCell } from "./ShinyTableCell";
 
 export interface ShinyStringCell extends ShinyTableCell {
