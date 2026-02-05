8f03a1e0ab0a191a26b51ba2f6aa17a9509e2a09
phlppchrtn <philippe.chretien@kleegroup.com>
2026-02-05 21:15:31 +0100

    mapping java - ts

--- banshee/src/models/input/toggle/ShinyToggle.ts
+++ banshee/src/models/input/toggle/ShinyToggle.ts
@@ -3,8 +3,7 @@
 // Any changes must be made in the corresponding Java file first.
 
 import { ShinyBlock } from '../../ShinyBlock';
-
-export type ShinyToggleType = 'TOGGLE' | 'CHECK' | 'SWITCH' | 'LIGHT' | 'BATTERY' | 'STATUS' | 'THUMBS' | 'ARROW' | 'STAR' | 'CLASSIC';
+import { ShinyToggleType } from './ShinyToggleType';
 
 export interface ShinyToggle extends ShinyBlock {
     label: string;
